506b8c885e8736a0c180265fe86ef977570dbaaf
devon <devonjhills@gmail.com>
2025-09-04 23:07:56 -0400

    Update .gitignore and astro.config.mjs for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://devonhills.dev',
   compressHTML: true,
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   build: {
     inlineStylesheets: 'auto',
