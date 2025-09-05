b9905c1f399fcebe2dace23b4ea624425b48708d
devon <devonjhills@gmail.com>
2025-09-04 22:55:58 -0400

    Configure Vercel deployment with Astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://devonhills.dev',
   compressHTML: true,
+  output: 'hybrid',
+  adapter: vercel(),
   build: {
     inlineStylesheets: 'auto',
   },
